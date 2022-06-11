ec4f21d39e45bb30219401a467df73aad94fed23
SlimeySlime <35423756+SlimeySlime@users.noreply.github.com>
2022-06-11 23:39:43 +0900

    jango setting

---
+++ polls/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return HttpResponse('Hello World poll project 1')
